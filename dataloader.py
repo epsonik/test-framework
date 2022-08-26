035289d4ca293999e4870a45a8ec5e3c97ee6a7c
mateuszb <mateusz.b@aibusters.com>
2022-08-26 22:49:23 +0200

    Update readme

--- dataloader.py
+++ dataloader.py
@@ -13,8 +13,8 @@ from pickle import dump, load
 from time import time
 from keras.preprocessing import sequence
 from keras.models import Sequential
-from keras.layers import LSTM, Embedding, TimeDistributed, Dense, RepeatVector,\
-                         Activation, Flatten, Reshape, concatenate, Dropout, BatchNormalization
+from keras.layers import LSTM, Embedding, TimeDistributed, Dense, RepeatVector, \
+    Activation, Flatten, Reshape, concatenate, Dropout, BatchNormalization
 from tensorflow.keras.optimizers import Adam, RMSprop
 from keras.layers.wrappers import Bidirectional
 from keras.layers.merge import add
@@ -35,9 +35,11 @@ import json
 from helper import *
 from pycocotools.coco import COCO
 from tqdm import tqdm
+
+
 class DataLoader:
     def __init__(self, config):
-        self.config=config
+        self.config = config
         if config["data_name"] is "flickr8k":
             self.flickr8k()
         if config["data_name"] is "flickr8k_polish":
@@ -53,49 +55,57 @@ class DataLoader:
         elif config["data_name"] is "coco17":
             self.coco_data()
 
-        
+
     def flickr8k(self):
-        
+        # load training dataset (6K)
+        filename = self.config["train_images_path"]
+        self.train = load_set(filename)
+        print("Train dataset")
+        #       id obrazow
+
+        print('Train dataset loaded: %d' % len(self.train))
         # Below path contains all the images
         self.images = self.config["images_path"]
-        # Create a list of all image names in the directory
+        # treningowy
         self.img = glob.glob(self.images + '*.jpg')
-        self.train_img, self.train_images = self.images_with_path(self.config["train_images_path"])  
-        
+        self.train_img, self.train_images = self.images_with_path(self.config["train_images_path"])
+
+        # zbior testowy
         self.test_img, self.test_images = self.images_with_path(self.config["test_images_path"])
         self.image_features_train, self.image_features_test = self.prepare_images(self.train_img, self.test_img)
         print('Photos: train=%d' % len(self.image_features_train))
-        
-        
+
         # załaduj opisy
-        self.doc = load_doc(self.config["token_path"])
+
         # parse descriptions
-        self.descriptions = load_descriptions(self.doc)
+        self.descriptions = load_descriptions(self.config["token_path"])
         print('Loaded: %d ' % len(self.descriptions))
-        
+
         clean_descriptions(self.descriptions, self.config)
         # summarize vocabulary
-        self.vocabulary = to_vocabulary(self.descriptions)
-        print('Original Vocabulary Size: %d' % len(self.vocabulary))
         print("Save descriptions in separate file")
         save_descriptions(self.descriptions, self.config["preprocessed_descriptions_save_path"])
-        
+
         # descriptions
-        self.train_descriptions = load_clean_descriptions(self.config["preprocessed_descriptions_save_path"], self.train)
+        self.train_descriptions = load_clean_descriptions(self.config["preprocessed_descriptions_save_path"],
+                                                          self.train)
         print('Descriptions: train=%d' % len(self.train_descriptions))
+        self.out()
+
+    def out(self):
         self.all_train_captions = self.all_train_captions(self.train_descriptions)
         print("Number of training captions ", len(self.all_train_captions))
         # determine the maximum sequence length
         self.max_length = max_length(self.train_descriptions)
         print('Description Length: %d' % self.max_length)
         # Count words and consider only words which occur at least 10 times in the corpus
-        self.vocab= self.count_words_and_threshold(self.all_train_captions)
+        self.vocab = self.count_words_and_threshold(self.all_train_captions)
         self.ixtoword, self.wordtoix = self.ixtowordandbackward(self.vocab)
-        self.vocab_size = len(self.ixtoword) + 1 # one for appended 0's
+        self.vocab_size = len(self.ixtoword) + 1  # one for appended 0's
         print("Vocab size: ", self.vocab_size)
-        
+
         self.embedding_matrix, self.embedding_vector = self.embedding_matrix(self.vocab_size, self.wordtoix)
-        
+
     def all_train_captions(self, train_descriptions):
         # Create a list of all the training captions
         all_train_captions = []
@@ -103,18 +113,18 @@ class DataLoader:
             for cap in val:
                 all_train_captions.append(cap)
         return all_train_captions
-    
+
     def images_with_path(self, images_path):
         # Below file conatains the names of images to be used in test/train data
         # Read the validation image names in a set# Read the test/train image names in a set
-        list_images = set(open(images_path, 'r').read().strip().split('\n'))        
+        list_images = set(open(images_path, 'r').read().strip().split('\n'))
         # Create a list of all the test/train images with their full path names
         list_img = []
-        for i in self.img: # img is list of full path names of all images
-            if i[len(self.images):] in list_images: # Check if the image belongs to test/training set
-                list_img.append(i) # Add it to the list of test/train images
+        for i in self.img:  # img is list of full path names of all images
+            if i[len(self.images):] in list_images:  # Check if the image belongs to test/training set
+                list_img.append(i)  # Add it to the list of test/train images
         return list_img, list_images
-        
+
     def count_words_and_threshold(self, all_train_captions):
         word_count_threshold = 10
         word_counts = {}
@@ -126,8 +136,8 @@ class DataLoader:
         # Consider only words which occur at least 10 times in the corpus
         vocab = [w for w in word_counts if word_counts[w] >= word_count_threshold]
         print('preprocessed words %d -> %d' % (len(word_counts), len(vocab)))
-        return vocab        
-    
+        return vocab
+
     def ixtowordandbackward(self, vocab):
         if self.config["save_ix_to_word"]:
             ixtoword = {}
@@ -140,15 +150,15 @@ class DataLoader:
             with open(self.config["ixtoword_path"], "wb") as encoded_pickle:
                 pickle.dump(ixtoword, encoded_pickle)
             with open(self.config["wordtoix_path"], "wb") as encoded_pickle:
-                pickle.dump(wordtoix, encoded_pickle)        
+                pickle.dump(wordtoix, encoded_pickle)
             return ixtoword, wordtoix
-    
+
         with open(self.config["ixtoword_path"], "rb") as encoded_pickle:
             ixtoword = load(encoded_pickle)
         with open(self.config["wordtoix_path"], "rb") as encoded_pickle:
             wordtoix = load(encoded_pickle)
         return ixtoword, wordtoix
-        
+
     def prepare_images(self, train_img, test_img):
         # Call the funtion to encode all the train images
         # This will take a while on CPU - Execute this only once
@@ -158,49 +168,47 @@ class DataLoader:
             encoding_test = {}
             index = 1
             for img in test_img:
-                image_filename = img.rsplit( "/", 1 )[-1]
+                image_filename = img.rsplit("/", 1)[-1]
                 encoding_test[image_filename] = encode(img, images_feature_model)
                 if index % 100 == 0:
                     print(index)
-            if not os.path.isdir("./"+self.config["data_name"]):
-                os.makedirs("./"+self.config["data_name"])
-                os.makedirs("./"+self.config["data_name"]+"/Pickle")
+            if not os.path.isdir("./" + self.config["data_name"]):
+                os.makedirs("./" + self.config["data_name"])
+                os.makedirs("./" + self.config["data_name"] + "/Pickle")
             with open(self.config["encoded_images_test"], "wb") as encoded_pickle:
                 pickle.dump(encoding_test, encoded_pickle)
             print(encoding_test)
-            print("Test images encoded")    
-            print("Time taken in seconds =", time()-start)
-            
+            print("Test images encoded")
+            print("Time taken in seconds =", time() - start)
+
             start = time()
             encoding_train = {}
             index = 1
             for img in train_img:
-                image_filename = img.rsplit( "/", 1 )[-1]
+                image_filename = img.rsplit("/", 1)[-1]
                 encoding_train[image_filename] = encode(img, images_feature_model)
                 if index % 100 == 0:
                     print(index)
-                index +=1
+                index += 1
             mode = 'a' if os.path.exists(self.config["encoded_images_train"]) else 'wb'
             # Save the bottleneck train features to disk
             with open(self.config["encoded_images_train"], 'w+b') as encoded_pickle:
                 pickle.dump(encoding_train, encoded_pickle)
-            print("Train images encoded")    
-            print("Time taken in seconds =", time()-start)
-           
-            
-            return encoding_train, encoding_test 
-        
+            print("Train images encoded")
+            print("Time taken in seconds =", time() - start)
+
+            return encoding_train, encoding_test
+
         with open(self.config["encoded_images_train"], "rb") as encoded_pickle:
             encoding_train = load(encoded_pickle)
         with open(self.config["encoded_images_test"], "rb") as encoded_pickle:
             encoding_test = load(encoded_pickle)
-        return encoding_train, encoding_test   
-            
-            
+        return encoding_train, encoding_test
+
     def embedding_matrix(self, vocab_size, wordtoix):
         # Load Glove vectors
 
-        embeddings_index = {} # empty dictionary
+        embeddings_index = {}  # empty dictionary
 
         f = open(self.config["word_embedings_path"], encoding="utf-8")
         for line in f:
@@ -223,44 +231,36 @@ class DataLoader:
         # Get 200-dim dense vector for each of the 10000 words in out vocabulary
         embedding_matrix = np.zeros((vocab_size, self.config["embedings_dim"]))
         for word, i in wordtoix.items():
-            #if i < max_words:
+            # if i < max_words:
             embedding_vector = embeddings_index.get(word)
             if embedding_vector is not None:
                 # Words not found in the embedding index will be all zeros
-                #1655,299 199
+                # 1655,299 199
                 embedding_matrix[i] = embedding_vector
         return embedding_matrix, embedding_vector
-    
+
     def coco_data(self):
         self.train_img, self.train_images, _, _ = self.load_coco_data()
 
+        _, _, self.test_img, self.test_images = self.load_coco_data()
         self.image_features_train, self.image_features_test = self.prepare_images(self.train_img, self.test_img)
-        
-        self.train_descriptions, self.descriptions = self.load_clean_descriptions_coco(self.config["token_path"], self.train_images)
+
+        self.train_descriptions, self.descriptions = self.load_clean_descriptions_coco(self.config["token_path"],
+                                                                                       self.train_images)
         print('Descriptions: train=%d' % len(self.train_descriptions))
-        self.all_train_captions = self.all_train_captions(self.train_descriptions)
-        print("Number of training captions ", len(self.all_train_captions))
-        # determine the maximum sequence length
-        self.max_length = max_length(self.train_descriptions)
-        print('Description Length: %d' % self.max_length)
-        # Count words and consider only words which occur at least 10 times in the corpus
-        self.vocab= self.count_words_and_threshold(self.all_train_captions)
-        self.ixtoword, self.wordtoix = self.ixtowordandbackward(self.vocab)
-        self.vocab_size = len(self.ixtoword) + 1 # one for appended 0's
-        print("Vocab size: ", self.vocab_size)
-        self.embedding_matrix, self.embedding_vector = self.embedding_matrix(self.vocab_size, self.wordtoix)
-        
+
+        self.out()
+
     def desc_raw(self, imgs, train_images):
         descriptions_raw = dict()
         for img in imgs:
-            image_filename= img['filename']
+            image_filename = img['filename']
             # create list
-            if image_filename.rsplit( ".", 1 )[0] not in descriptions_raw:
-                descriptions_raw[image_filename.rsplit( ".", 1 )[0]] = list()
+            if image_filename.rsplit(".", 1)[0] not in descriptions_raw:
+                descriptions_raw[image_filename.rsplit(".", 1)[0]] = list()
             for sent in img['sentences']:
-                descriptions_raw[image_filename.rsplit( ".", 1 )[0]].append(sent['raw'])
+                descriptions_raw[image_filename.rsplit(".", 1)[0]].append(sent['raw'])
         return descriptions_raw
-
 
     def load_clean_descriptions_coco(self, filename, train_images):
         imgs = json.load(open(filename, 'r'))
@@ -269,43 +269,43 @@ class DataLoader:
 
         if self.config["preprocess_descriptions"]:
             for img in imgs:
-                image_filename= img['filename']
+                image_filename = img['filename']
                 if image_filename in train_images:
                     # create list
-                    if image_filename.rsplit( ".", 1 )[0] not in descriptions:
-                        descriptions[image_filename.rsplit( ".", 1 )[0]] = list()
+                    if image_filename.rsplit(".", 1)[0] not in descriptions:
+                        descriptions[image_filename.rsplit(".", 1)[0]] = list()
                     for sent in img['sentences']:
                         # wrap descriion in tokens
                         desc = 'startseq ' + " ".join(sent['tokens']) + ' endseq'
                         # store
-                        descriptions[image_filename.rsplit( ".", 1 )[0]].append(desc)
+                        descriptions[image_filename.rsplit(".", 1)[0]].append(desc)
             with open(self.config["preprocessed_descriptions_save_path"], "wb") as encoded_pickle:
                 pickle.dump(descriptions, encoded_pickle)
-            
+
             return descriptions, self.desc_raw(imgs, train_images)
 
         with open(self.config["preprocessed_descriptions_save_path"], "rb") as encoded_pickle:
             descriptions = load(encoded_pickle)
-        
+
         return descriptions, self.desc_raw(imgs, train_images)
-    
+
     def load_coco_data(self):
         input_json = self.config["images_names_path"]
         images_folder = self.config["images_folder"]
         print('DataLoader loading json file: ', input_json)
         info = json.load(open(input_json))
         train_img = []
-        train_images=[]
-        test_img=[]
-        test_images=[]
+        train_images = []
+        test_img = []
+        test_images = []
         for ix in range(len(info['images'])):
             img = info['images'][ix]
             image_filename = img['file_path']
-            file_path =  images_folder + "/"+img['file_path']
-            
-            if image_filename.find("/") != -1 :
-                image_filename = img['file_path'].rsplit( "/", 1 )[ 1 ]
-            
+            file_path = images_folder + "/" + img['file_path']
+
+            if image_filename.find("/") != -1:
+                image_filename = img['file_path'].rsplit("/", 1)[1]
+
             if img['split'] == 'train':
                 train_img.append(file_path)
                 train_images.append(image_filename)
@@ -318,5 +318,5 @@ class DataLoader:
             elif img['split'] == 'restval':
                 train_img.append(file_path)
                 train_images.append(image_filename)
-                            
+
         return train_img, train_images, test_img, test_images
