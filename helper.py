476e3f47cc2d854433e105768409db14db90481d
mateuszb <mateusz.b@aibusters.com>
2022-08-26 23:04:18 +0200

    Update readme

--- helper.py
+++ helper.py
@@ -24,8 +24,8 @@ from pickle import dump, load
 from time import time
 from keras.preprocessing import sequence
 from keras.models import Sequential
-from keras.layers import LSTM, Embedding, TimeDistributed, Dense, RepeatVector,\
-                         Activation, Flatten, Reshape, concatenate, Dropout, BatchNormalization
+from keras.layers import LSTM, Embedding, TimeDistributed, Dense, RepeatVector, \
+    Activation, Flatten, Reshape, concatenate, Dropout, BatchNormalization
 from tensorflow.keras.optimizers import Adam, RMSprop
 from keras.layers.wrappers import Bidirectional
 from keras.layers.merge import add
@@ -44,111 +44,115 @@ from keras import callbacks
 from keras import optimizers
 from helper import *
 
-def load_doc(filename):
-	file = open(filename, 'r')
-	text = file.read()
-	file.close()
-	return text
 
-def load_descriptions(doc):
-	mapping = dict()
-	for line in doc.split('\n'):
-		tokens = line.split()
-		if len(line) < 2:
-			continue
-		# take the first token as the image id, the rest as the description
-		image_id, image_desc = tokens[0], tokens[1:]
-		# extract filename from image id
-		image_id = image_id.split('.')[0]
-		# convert description tokens back to string
-		image_desc = ' '.join(image_desc)
-		# create the list if needed
-		if image_id not in mapping:
-			mapping[image_id] = list()
-		# store description
-		mapping[image_id].append(image_desc)
-	return mapping
+def load_doc(filename):
+    file = open(filename, 'r')
+    text = file.read()
+    file.close()
+    return text
+
+
+def load_descriptions(token_path):
+    doc = load_doc(token_path)
+    mapping = dict()
+    for line in doc.split('\n'):
+        tokens = line.split()
+        if len(line) < 2:
+            continue
+        # take the first token as the image id, the rest as the description
+        image_id, image_desc = tokens[0], tokens[1:]
+        # extract filename from image id
+        image_id = image_id.split('.')[0]
+        # convert description tokens back to string
+        image_desc = ' '.join(image_desc)
+        # create the list if needed
+        if image_id not in mapping:
+            mapping[image_id] = list()
+        # store description
+        mapping[image_id].append(image_desc)
+    return mapping
 
 
 def clean_descriptions(descriptions, config):
-  # prepare translation table for removing punctuation
-  table = str.maketrans('', '', string.punctuation)
-  for key, desc_list in descriptions.items():
-    for i in range(len(desc_list)):
-      desc = desc_list[i]
-      if config["use_lemma"]:
-          doc = nlp(desc)
-      # tokenize
-      desc = desc.split()
-      # convert to lower case
-      desc = [word.lower() for word in desc]
-# Lematyzacja
-#### spacy model from here: http://zil.ipipan.waw.pl/SpacyPL?action=AttachFile&do=view&target=pl_spacy_model-0.1.0.tar.gz
-#### spacy dokumentacja - https://spacy.io/usage
-      if config["use_lemma"]:
-          desc = [word.lemma_ for word in doc]
-      # remove punctuation from each token
-      desc = [w.translate(table) for w in desc]
-      # remove tokens with numbers in them
-      desc = [word for word in desc if word.isalpha()]
-      # store as string
-      desc_list[i] =  ' '.join(desc)
-        
+    # prepare translation table for removing punctuation
+    table = str.maketrans('', '', string.punctuation)
+    for key, desc_list in descriptions.items():
+        for i in range(len(desc_list)):
+            desc = desc_list[i]
+            # tokenize
+            desc = desc.split()
+            # convert to lower case
+            desc = [word.lower() for word in desc]
+            # Lematyzacja
+            #### spacy model from here: http://zil.ipipan.waw.pl/SpacyPL?action=AttachFile&do=view&target=pl_spacy_model-0.1.0.tar.gz
+            #### spacy dokumentacja - https://spacy.io/usage
+            if config["use_lemma"]:
+                desc = [word.lemma_ for word in doc]
+            # remove punctuation from each token
+            desc = [w.translate(table) for w in desc]
+            # remove tokens with numbers in them
+            desc = [word for word in desc if word.isalpha()]
+            # store as string
+            desc_list[i] = ' '.join(desc)
+
+
 # convert the loaded descriptions into a vocabulary of words
 def to_vocabulary(descriptions):
-	# build a list of all description strings
-	all_desc = set()
-	for key in descriptions.keys():
-		[all_desc.update(d.split()) for d in descriptions[key]]
-	return all_desc
+    # build a list of all description strings
+    all_desc = set()
+    for key in descriptions.keys():
+        [all_desc.update(d.split()) for d in descriptions[key]]
+    return all_desc
 
 
 # save descriptions to file, one per line
 def save_descriptions(descriptions, filename):
-	lines = list()
-	for key, desc_list in descriptions.items():
-		for desc in desc_list:
-			lines.append(key + ' ' + desc)
-	data = '\n'.join(lines)
-	file = open(filename, 'w')
-	file.write(data)
-	file.close()
-    
-    
+    lines = list()
+    for key, desc_list in descriptions.items():
+        for desc in desc_list:
+            lines.append(key + ' ' + desc)
+    data = '\n'.join(lines)
+    file = open(filename, 'w')
+    file.write(data)
+    file.close()
+
+
 # load a pre-defined list of photo identifiers
 def load_set(filename):
-	doc = load_doc(filename)
-	dataset = list()
-	# process line by line
-	for line in doc.split('\n'):
-		# skip empty lines
-		if len(line) < 1:
-			continue
-		# get the image identifier
-		identifier = line.split('.')[0]
-		dataset.append(identifier)
-	return set(dataset)
+    doc = load_doc(filename)
+    dataset = list()
+    # process line by line
+    for line in doc.split('\n'):
+        # skip empty lines
+        if len(line) < 1:
+            continue
+        # get the image identifier
+        identifier = line.split('.')[0]
+        dataset.append(identifier)
+    return set(dataset)
+
 
 # load clean descriptions into memory
 def load_clean_descriptions(filename, dataset):
-	# load document
-	doc = load_doc(filename)
-	descriptions = dict()
-	for line in doc.split('\n'):
-		# split line by white space
-		tokens = line.split()
-		# split id from description
-		image_id, image_desc = tokens[0], tokens[1:]
-		# skip images not in the set
-		if image_id in dataset:
-			# create list
-			if image_id not in descriptions:
-				descriptions[image_id] = list()
-			# wrap description in tokens
-			desc = 'startseq ' + ' '.join(image_desc) + ' endseq'
-			# store
-			descriptions[image_id].append(desc)
-	return descriptions
+    # load document
+    doc = load_doc(filename)
+    descriptions = dict()
+    for line in doc.split('\n'):
+        # split line by white space
+        tokens = line.split()
+        # split id from description
+        image_id, image_desc = tokens[0], tokens[1:]
+        # skip images not in the set
+        if image_id in dataset:
+            # create list
+            if image_id not in descriptions:
+                descriptions[image_id] = list()
+            # wrap description in tokens
+            desc = 'startseq ' + ' '.join(image_desc) + ' endseq'
+            # store
+            descriptions[image_id].append(desc)
+    return descriptions
+
 
 def preprocess(image_path):
     # Convert all the images to size 299x299 as expected by the inception v3 model
@@ -161,12 +165,14 @@ def preprocess(image_path):
     x = preprocess_input(x)
     return x
 
+
 # Function to encode a given image into a vector of size (2048, )
 def encode(image, images_feature_model):
-    image = preprocess(image) # preprocess the image
-    fea_vec = images_feature_model.predict(image) # Get the encoding vector for the image
-    fea_vec = np.reshape(fea_vec, fea_vec.shape[1]) # reshape from (1, 2048) to (2048, )
+    image = preprocess(image)  # preprocess the image
+    fea_vec = images_feature_model.predict(image)  # Get the encoding vector for the image
+    fea_vec = np.reshape(fea_vec, fea_vec.shape[1])  # reshape from (1, 2048) to (2048, )
     return fea_vec
+
 
 def define_images_feature_model():
     # Load the inception v3 model
@@ -175,28 +181,31 @@ def define_images_feature_model():
     model_new = Model(model.input, model.layers[-2].output)
     return model_new
 
+
 # convert a dictionary of clean descriptions to a list of descriptions
 def to_lines(descriptions):
-	all_desc = list()
-	for key in descriptions.keys():
-		[all_desc.append(d) for d in descriptions[key]]
-	return all_desc
+    all_desc = list()
+    for key in descriptions.keys():
+        [all_desc.append(d) for d in descriptions[key]]
+    return all_desc
+
 
 # calculate the length of the description with the most words
 def max_length(descriptions):
-	lines = to_lines(descriptions)
-	return max(len(d.split()) for d in lines)
+    lines = to_lines(descriptions)
+    return max(len(d.split()) for d in lines)
+
 
 # data generator, intended to be used in a call to model.fit_generator()
 def data_generator(descriptions, photos, wordtoix, max_length, num_photos_per_batch, vocab_size):
     X1, X2, y = list(), list(), list()
-    n=0
+    n = 0
     # loop for ever over images
     while 1:
         for key, desc_list in descriptions.items():
-            n+=1
+            n += 1
             # retrieve the photo feature
-            photo = photos[key+'.jpg']
+            photo = photos[key + '.jpg']
             for desc in desc_list:
                 # encode the sequence
                 seq = [wordtoix[word] for word in desc.split(' ') if word in wordtoix]
@@ -213,36 +222,36 @@ def data_generator(descriptions, photos, wordtoix, max_length, num_photos_per_ba
                     X2.append(in_seq)
                     y.append(out_seq)
             # yield the batch data
-            if n==num_photos_per_batch:
-#                 yield [[array(X1), array(X2)], array(y)]
-                yield ([array(X1), array(X2)], array(y)) 
+            if n == num_photos_per_batch:
+                #                 yield [[array(X1), array(X2)], array(y)]
+                yield ([array(X1), array(X2)], array(y))
                 X1, X2, y = list(), list(), list()
-                n=0
-            
-            
+                n = 0
+
+
 def isfloat(value):
-  try:
-    float(value)
-    return True
-  except ValueError:
-    return False
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
 
 
 def prepare_for_evaluation(encoding_test, data, model):
     from tqdm.notebook import tqdm
     test_pics = list(encoding_test.keys())
-    expected={}
-    results={}
-    #calculation of metrics for test images dataset
-    for j in tqdm(range(0,len(test_pics))):
+    expected = {}
+    results = {}
+    # calculation of metrics for test images dataset
+    for j in tqdm(range(0, len(test_pics))):
         pic = test_pics[j]
-        image_id=pic.rsplit( ".", 1 )[ 0 ].rsplit("_",1)[0]
-        expected[image_id]=[]
-        image = encoding_test[pic].reshape((1,2048))
-        for desc in data.descriptions[pic.rsplit( ".", 1 )[ 0 ]]:
-            expected[image_id].append({"image_id":image_id, "caption":desc})
+        image_id = pic.rsplit(".", 1)[0].rsplit("_", 1)[0]
+        expected[image_id] = []
+        image = encoding_test[pic].reshape((1, 2048))
+        for desc in data.descriptions[pic.rsplit(".", 1)[0]]:
+            expected[image_id].append({"image_id": image_id, "caption": desc})
         generated = greedySearch(image, model, data.wordtoix, data.ixtoword, data.max_length)
-        results[image_id]=[{"image_id":image_id, "caption":generated}]
+        results[image_id] = [{"image_id": image_id, "caption": generated}]
     return expected, results
 
 
@@ -251,7 +260,7 @@ def greedySearch(photo, model, wordtoix, ixtoword, max_length):
     for i in range(max_length):
         sequence = [wordtoix[w] for w in in_text.split() if w in wordtoix]
         sequence = pad_sequences([sequence], maxlen=max_length)
-        yhat = model.predict([photo,sequence], verbose=0)
+        yhat = model.predict([photo, sequence], verbose=0)
         yhat = np.argmax(yhat)
         word = ixtoword[yhat]
         in_text += ' ' + word
@@ -260,5 +269,4 @@ def greedySearch(photo, model, wordtoix, ixtoword, max_length):
     final = in_text.split()
     final = final[1:-1]
     final = ' '.join(final)
-    return final   
-    
+    return final
