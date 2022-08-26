4caaccab3d821d5951d4b0d3d10bdb22825311fc
mateuszb <mateusz.b@aibusters.com>
2022-08-26 22:00:07 +0200

    Update readme

--- helper.py
+++ helper.py
@@ -228,23 +228,6 @@ def isfloat(value):
     return False
 
 
-def greedySearch(photo):
-    in_text = 'startseq'
-    for i in range(max_length):
-        sequence = [wordtoix[w] for w in in_text.split() if w in wordtoix]
-        sequence = pad_sequences([sequence], maxlen=max_length)
-        yhat = model.predict([photo,sequence], verbose=0)
-        yhat = np.argmax(yhat)
-        word = ixtoword[yhat]
-        in_text += ' ' + word
-        if word == 'endseq':
-            break
-    final = in_text.split()
-    final = final[1:-1]
-    final = ' '.join(final)
-    return final
-
-
 def prepare_for_evaluation(encoding_test, data, model):
     from tqdm.notebook import tqdm
     test_pics = list(encoding_test.keys())
