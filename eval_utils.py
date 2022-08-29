f18e13ee460e672b282542f8692a6465638ff8f0
mateuszb <mateusz.b@aibusters.com>
2022-08-29 23:28:23 +0200

    report

--- eval_utils.py
+++ eval_utils.py
@@ -1,16 +1,7 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
-import torch
-import torch.nn as nn
-
-import numpy as np
 import json
-from json import encoder
-import random
-import string
-import time
 import os
 import sys
 
--- helper.py
+++ helper.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 import json
 import os
+import string
 
 import numpy as np
 from keras.applications.inception_v3 import InceptionV3
