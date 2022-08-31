fbb643435b3e3e99b35851baa8370ab38b6cde28
epsonik <epsonik@o2.pl>
2022-09-01 00:51:57 +0200

    T

--- config.py
+++ config.py
@@ -18,6 +18,24 @@ config_mixed_flickr8k_flickr8k = {
     "model_save_path": "/model_weights/model_Base_3_Batch_Komninos.h5",
     "data_name": "mixed_flickr8k_30k",
 }
+config_mixed_flickr8k_polish_flickr8k_polish = {
+    "train": {"dataset_name": "flickr8k_polish", "subset_name": "test"},
+    "test": {"dataset_name": "flickr8k_polish", "subset_name": "train"},
+    "train_model": True,
+    "save_model": True,
+    "model_save_dir": "/model_weights/",
+    "model_save_path": "/model_weights/model_Base_3_Batch_Komninos.h5",
+    "data_name": "mixed_flickr8k_polish_flickr8k_polish",
+}
+config_mixed_aide_aide = {
+    "train": {"dataset_name": "aide", "subset_name": "test"},
+    "test": {"dataset_name": "aide", "subset_name": "train"},
+    "train_model": True,
+    "save_model": True,
+    "model_save_dir": "/model_weights/",
+    "model_save_path": "/model_weights/model_Base_3_Batch_Komninos.h5",
+    "data_name": "mixed_aide_aide",
+}
 config_mixed_coco2017_coco2017 = {
     "train": {"dataset_name": "coco17", "subset_name": "test"},
     "test": {"dataset_name": "coco17", "subset_name": "train"},
@@ -26,6 +44,15 @@ config_mixed_coco2017_coco2017 = {
     "model_save_dir": "/model_weights/",
     "model_save_path": "/model_weights/model_Base_3_Batch_Komninos.h5",
     "data_name": "mixed_coco2017_coco2017",
+}
+config_mixed_coco2014_coco2014 = {
+    "train": {"dataset_name": "coco14", "subset_name": "test"},
+    "test": {"dataset_name": "coco14", "subset_name": "train"},
+    "train_model": True,
+    "save_model": True,
+    "model_save_dir": "/model_weights/",
+    "model_save_path": "/model_weights/model_Base_3_Batch_Komninos.h5",
+    "data_name": "mixed_coco2014_coco2014",
 }
 config_mixed_coco2017_flickr8k = {
     "train": {"dataset_name": "coco17", "subset_name": "test"},
