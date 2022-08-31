deaa8b8544ca58e4135017d61b0e735117f7c65c
mateuszb <mateusz.b@aibusters.com>
2022-08-31 23:29:28 +0200

    update dataloader

--- config.py
+++ config.py
@@ -18,7 +18,15 @@ config_mixed_flickr8k_flickr8k = {
     "model_save_path": "/model_weights/model_Base_3_Batch_Komninos.h5",
     "data_name": "mixed_flickr8k_30k",
 }
-
+config_mixed_coco2017_coco2017 = {
+    "train": {"dataset_name": "coco2017", "subset_name": "test"},
+    "test": {"dataset_name": "coco2017", "subset_name": "train"},
+    "train_model": True,
+    "save_model": True,
+    "model_save_dir": "/model_weights/",
+    "model_save_path": "/model_weights/model_Base_3_Batch_Komninos.h5",
+    "data_name": "mixed_coco2017_coco2017",
+}
 config_mixed_flickr8k_coco2017 = {
     "train": {"dataset_name": "flickr8k", "subset_name": "test"},
     "test": {"dataset_name": "coco2017", "subset_name": "train"},
