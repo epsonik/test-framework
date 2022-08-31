599bc6283b9961d05a000129660d4439311525ba
epsonik <epsonik@o2.pl>
2022-08-31 23:34:35 +0200

    T

--- config.py
+++ config.py
@@ -18,23 +18,12 @@ config_mixed_flickr8k_flickr8k = {
     "model_save_path": "/model_weights/model_Base_3_Batch_Komninos.h5",
     "data_name": "mixed_flickr8k_30k",
 }
-
-config_mixed_flickr8k_coco2017 = {
-    "train": {"dataset_name": "flickr8k", "subset_name": "test"},
+config_mixed_coco2017_coco2017 = {
+    "train": {"dataset_name": "coco2017", "subset_name": "test"},
     "test": {"dataset_name": "coco2017", "subset_name": "train"},
     "train_model": True,
     "save_model": True,
     "model_save_dir": "/model_weights/",
     "model_save_path": "/model_weights/model_Base_3_Batch_Komninos.h5",
-    "data_name": "mixed_flickr8k_coco2017",
-}
-
-config_flickr8k_30k = {
-    "train_images": {"dataset_name": "flickr8k", "subset_name": "train"},
-    "test_images": {"dataset_name": "flickr30k", "subset_name": "test"},
-    "train_model": True,
-    "save_model": True,
-    "model_save_dir": "/model_weights/",
-    "model_save_path": "/model_weights/model_Base_3_Batch_Komninos.h5",
-    "data_name": "mixed_flickr8k_30k",
+    "data_name": "mixed_coco2017_coco2017",
 }
