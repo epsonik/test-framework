5e35f721c320b83140a6ec09ecadc0a75fddfc00
mateuszb <mateusz.b@aibusters.com>
2023-01-17 20:27:03 +0100

    h

--- data_processor.py
+++ data_processor.py
@@ -86,11 +86,11 @@ def define_images_feature_model(images_processor):
         name of the image processor
     """
     if images_processor == 'vgg16':
-        model_images_processor_name = VGG16(weights='imagenet', include_top=False)
+        model_images_processor_name = VGG16(weights='imagenet', include_top=False, input_shape=(299, 299, 3))
         from keras.applications.inception_v3 import preprocess_input
         print("Used: vgg16")
     elif images_processor == 'EfficientNetB7':
-        model_images_processor_name = EfficientNetB7(weights='imagenet', include_top=False)
+        model_images_processor_name = EfficientNetB7(weights='imagenet', include_top=False, input_shape=(299, 299, 3))
         from keras.applications.efficientnet import preprocess_input
         print("Used: EfficientNetB7")
     elif images_processor == 'Xception':
