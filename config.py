7f7fe13a8de9c5ed1699b9d68d01bc08ff2b5035
mateuszb <mateusz.b@aibusters.com>
2023-02-02 09:02:51 +0100

    add fasttext

--- config.py
+++ config.py
@@ -299,8 +299,8 @@ config_mixed_flickr8k_flickr8k_inception_fastText = {
 config_mixed_coco14_coco14_inception_fastText = {
     "train": {"dataset_name": "coco14", "subset_name": "train"},
     "test": {"dataset_name": "coco14", "subset_name": "test"},
-    "encode_images": True,
-    "save_ix_to_word": True,
+    "encode_images": False,
+    "save_ix_to_word": False,
     "train_model": True,
     "save_model": True,
     "ixtoword_path": "ixtoword.pkl",
