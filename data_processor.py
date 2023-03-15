4eaefc71831e048fe3829fbcd73a8130cf3a2173
mateuszb <mateusz.b@aibusters.com>
2023-03-15 19:31:13 +0100

    Revert "oneHot"
    This reverts commit fa3767c1

--- data_processor.py
+++ data_processor.py
@@ -502,7 +502,11 @@ def preprocess_data(data):
 
 
 def get_oneHot_embedding_matrix(vocab_size):
-    return None
+    from tensorflow.keras.utils import to_categorical
+    embedding_matrix = to_categorical(list(range(0, vocab_size - 1)))
+    embedding_matrix = np.vstack([[0] * (vocab_size - 1),embedding_matrix])
+    print(embedding_matrix)
+    return embedding_matrix
 
 
 def get_fast_text_embedding_matrix(vocab_size, wordtoix, word_embedings_path, embedings_dim):
